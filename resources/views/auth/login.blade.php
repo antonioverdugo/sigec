13ceaaef2d5ae7bff9bdd436474507b7bbebec4f
antonioverdugo <antonioverdugogarcia@gmail.com>
2026-03-30 17:44:42 +0200

    Modificacion de el formulario de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,11 +29,6 @@
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recuerdame') }}</span>
             </label>
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('¿Olvidaste tu contraseña?') }}
-                </a>
-            @endif
         </div>
 
         <!-- Botón de inicio de sesión -->
